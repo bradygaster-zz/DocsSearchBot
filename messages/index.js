437caa25ae53d9e3f8ef55c9f3b4181537e53822
Brady Gaster <bradyg@microsoft.com>
2016-12-10 03:19:42 -0800

    added multiple dialogs

--- messages/index.js
+++ messages/index.js
@@ -16,8 +16,16 @@ var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', function (session) {
     if (session.message.text == 'help')
-        bot.beginDialog('/fre')
-        
+        bot.beginDialog('/fre');
+    else
+        bot.beginDialog('/search');
+});
+
+bot.dialog('/fre', function (session) {
+    session.send('Welcome to the docs.microsoft.com search bot. Simply type in a search term and I\'ll find you relevant links from our documentation.');
+});
+
+bot.dialog('/search', function (session) {
     docs.searchDocs(session.message.text)
         .then((result) => {
             docs.createReplyFromResults(result.results, result.searched)
@@ -26,11 +34,6 @@ bot.dialog('/', function (session) {
                 });
         });
 });
-
-bot.dialog('/fre', function (session) {
-    session.send('Welcome to the docs.microsoft.com search bot. Simply type in a search term and I\'ll find you relevant links from our documentation.');
-});
-
 
 if (useEmulator) {
     var restify = require('restify');
